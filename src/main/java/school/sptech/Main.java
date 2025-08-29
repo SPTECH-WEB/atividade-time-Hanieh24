6a5e9a6016faa10cd972f85fd6d4af2ad52bd373
Hanieh24 <hanieh.ashouri@sptech.school>
2025-08-29 14:25:24 -0300

    atividade concluido

--- src/main/java/school/sptech/Main.java
+++ src/main/java/school/sptech/Main.java
@@ -3,6 +3,52 @@ package school.sptech;
 public class Main {
 
     public static void main(String[] args) {
+        Time estrela = new Time();
+        estrela.nome="Estrela do Norte";
+        estrela.vitorias=10;
+        estrela.empates=5;
+        estrela.derrotas=2;
+        estrela.exibirInformacoes();
 
+        Time aguia = new Time();
+        aguia.nome="Águia Branca";
+        aguia.vitorias=7;
+        aguia.empates=4;
+        aguia.derrotas=6;
+        aguia.exibirInformacoes();
+
+        estrela.compararAproveitamento(aguia);
+
+        Time vento = new Time();
+        vento.nome="Vento Forte";
+        vento.vitorias=2;
+        vento.empates=3;
+        vento.derrotas=12;
+        vento.exibirInformacoes();
+
+        Time mare = new Time();
+        mare.nome="Maré Alta";
+        mare.vitorias=5;
+        mare.empates=5;
+        mare.derrotas=7;
+        mare.exibirInformacoes();
+
+        vento.compararAproveitamento(mare);
+
+        Time rocha = new Time();
+        rocha.nome="Rocha sólida";
+        rocha.vitorias=4;
+        rocha.empates=3;
+        rocha.derrotas=10;
+        rocha.exibirInformacoes();
+
+        Time fogo = new Time();
+        fogo.nome="Fogo Ardente";
+        fogo.vitorias=4;
+        fogo.empates=5;
+        fogo.derrotas=8;
+        fogo.exibirInformacoes();
+
+        rocha.compararAproveitamento(fogo);
     }
 }
--- src/main/java/school/sptech/Time.java
+++ src/main/java/school/sptech/Time.java
@@ -1,5 +1,68 @@
 package school.sptech;
 
 public class Time {
+    String nome;
+    int vitorias;
+    int empates;
+    int derrotas;
+
+    public Time(){
+        vitorias = 0;
+        empates =0;
+        derrotas =0;
+    }
+
+    public void registrarVitoria(){
+        vitorias++;
+    }
+
+    public void registrarEmpate(){
+        empates++;
+    }
+
+    public void registrarDerrota(){
+        derrotas++;
+    }
+
+    public int getPontos(){
+        int pontos=(vitorias*3)+(empates);
+        return (pontos);
+    }
+
+    public int getTotalPartidas(){
+        int partidas = vitorias + empates + derrotas;
+        return (partidas);
+    }
+
+    public int getAproveitamento(){
+        int partidas= getTotalPartidas();
+        int aproveitamento = (vitorias*100)/partidas;
+        return (aproveitamento);
+    }
+
+    public void compararAproveitamento(Time adversario){
+        int aproveitamentoP=adversario.getAproveitamento();
+        int aproveitamento=getAproveitamento();
+        if (aproveitamento>aproveitamentoP){
+            System.out.println("O time "+ nome + " tem um aproveitamento maior que o time " + adversario.nome);
+        } else if (aproveitamento<aproveitamentoP) {
+            System.out.println("O time "+ adversario.nome + " tem um aproveitamento maior que o time " +nome);
+        } else{
+            System.out.println("Os times "+ nome +" e "+ adversario.nome +" têm o mesmo aproveitamento");
+        }
+    }
+
+    public void exibirInformacoes(){
+        System.out.println("Time: " + nome);
+        System.out.println("Vitória: " + vitorias);
+        System.out.println("Empates: " + empates);
+        System.out.println("Derrotas: " + derrotas);
+        System.out.println(" ");
+        System.out.println("Pontos: " + getPontos());
+        System.out.println("Total de Partidas: " + getTotalPartidas());
+        System.out.println("Aproveitamento: " + getAproveitamento() + "%");
+        System.out.println("___");
+    }
+
 
 }
